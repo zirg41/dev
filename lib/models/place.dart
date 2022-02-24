669b8480cf098c8dac2990cbdf9449eafcd7009d
Фаиз <galimow63@gmail.com>
2022-02-24 22:52:08 +0400

    place model added

---
+++ lib/models/place.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+class Place {
+  final String id;
+  final String title;
+  final PlaceLocation location;
+  final File image;
+
+  Place({
+    @required this.id,
+    @required this.image,
+    @required this.location,
+    @required this.title,
+  });
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  final String addres;
+
+  PlaceLocation({
+    @required this.addres,
+    @required this.latitude,
+    @required this.longitude,
+  });
+}
