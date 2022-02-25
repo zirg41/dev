3f2e308c96e6f06dae48b4ccb4072497b4fd4dab
Фаиз <galimow63@gmail.com>
2022-02-25 23:14:56 +0400

    AddPlaceScreen managed

---
+++ lib/screens/add_place_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/widgets/image_input.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = "/add-place";
+  AddPlaceScreen();
+
+  @override
+  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add a New place"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(labelText: "Title"),
+                    controller: _titleController,
+                  ),
+                  SizedBox(height: 10),
+                  ImageInput(),
+                ],
+              ),
+            ),
+          ),
+          Spacer(),
+          RaisedButton.icon(
+            icon: Icon(Icons.add),
+            label: Text("Add place"),
+            onPressed: () {},
+            elevation: 0,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            color: Theme.of(context).accentColor,
+          ),
+        ],
+      ),
+    );
+  }
+}
