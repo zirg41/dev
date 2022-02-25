dd744920f8bb80a25d50969ee79c8640cfbc0d42
Фаиз <galimow63@gmail.com>
2022-02-26 00:06:58 +0400

    using path and path provider packages

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,6 +2,8 @@ import 'dart:io' as io;
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
   const ImageInput();
@@ -22,6 +24,10 @@ class _ImageInputState extends State<ImageInput> {
     setState(() {
       _storedImage = io.File(imageFile.path);
     });
+
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await _storedImage.copy("${appDir.path}/$fileName");
   }
 
   @override
