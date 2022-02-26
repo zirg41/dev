8ed9f0b4ce0a277551d509a9499dc807473e7b29
Фаиз <galimow63@gmail.com>
2022-02-26 19:08:49 +0400

    insert method to SQLite database added

---
+++ lib/helpers/db_helper.dart
@@ -0,0 +1,21 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+class DBHelper {
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final dbPath = await sql.getDatabasesPath();
+    final sqlDB = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      version: 1,
+      onCreate: (db, version) {
+        return db.execute(
+            "CREATE TABLE user_places(id TEXT PRIMARY, title TEXT, image TEXT");
+      },
+    );
+    await sqlDB.insert(
+      table,
+      data,
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
+    );
+  }
+}
--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_complete_guide/helpers/db_helper.dart';
 import 'package:image_picker/image_picker.dart';
 import '/models/place.dart';
 
@@ -19,5 +20,10 @@ class GreatPlaces with ChangeNotifier {
         title: pickedTitle);
     _items.add(newPlace);
     notifyListeners();
+    DBHelper.insert("places", {
+      "id": newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
   }
 }
