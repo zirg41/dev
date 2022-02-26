2245f425149ee16a10cd9cf1d67412e2fdb7fe44
Фаиз <galimow63@gmail.com>
2022-02-26 15:07:02 +0400

    addPlace added to provider

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,10 +1,22 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_complete_guide/models/place.dart';
+import '/models/place.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
 
   List<Place> get items {
     return [..._items];
+  }
+
+  void addPlace(String pickedTitle, File pickedImage) {
+    final newPlace = Place(
+        id: DateTime.now().toString(),
+        image: pickedImage,
+        location: null,
+        title: pickedTitle);
+    _items.add(newPlace);
+    notifyListeners();
   }
 }
